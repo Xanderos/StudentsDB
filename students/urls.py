c2acd92b064d601eba99a9112ad5f65abdc9e5f8
Xanderos <aleksandrlex85@gmail.com>
2018-09-11 11:52:23 +0300

    package view

--- students/urls.py
+++ students/urls.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from students.views import Students,Groups
 
 urlpatterns = [
-    path('',views.students_list,name='home'),
-    path('students/add/', views.students_add,name='students_add'),
-    path('students/<int:sid>/edit/',views.students_edit,name='students_edit'),
-    path('students/<int:sid>/delete/',views.students_delete,name='students_delete'),
+    path('',Students.students_list,name='home'),
+    path('students/add/', Students.students_add,name='students_add'),
+    path('students/<int:sid>/edit/',Students.students_edit,name='students_edit'),
+    path('students/<int:sid>/delete/',Students.students_delete,name='students_delete'),
 
-    path('groups/', views.groups_list, name='groups'),
-    path('groups/add/',views.groups_add,name='groups_add'),
-    path('groups/(<int:gid>/edit/',views.groups_edit,name='groups_edit'),
-    path('groups/(<int:gid>/delete/',views.groups_delete,name='groups_delete'),
+    path('groups/', Groups.groups_list, name='groups'),
+    path('groups/add/',Groups.groups_add,name='groups_add'),
+    path('groups/(<int:gid>/edit/',Groups.groups_edit,name='groups_edit'),
+    path('groups/(<int:gid>/delete/',Groups.groups_delete,name='groups_delete'),
     ]
--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -68,6 +68,8 @@ TEMPLATES = [
     },
 ]
 
+
+
 WSGI_APPLICATION = 'studentsdb.wsgi.application'
 
 
@@ -120,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static",)),
