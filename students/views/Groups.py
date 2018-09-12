029bc704b9cb7f8817bdfb0c2f559574e1b188b8
Xanderos <aleksandrlex85@gmail.com>
2018-09-12 18:08:01 +0300

    package model

--- students/views/Groups.py
+++ students/views/Groups.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from ..models import Group
+from ..models.groups import Group
+from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 
 def groups_list(request):
-     return HttpResponse('<h1>Groups Listing</h1>')
+    groups=Group.objects.all()
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('title', 'leader', 'id'):
+        groups = groups.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            groups = groups.reverse()
+    paginator = Paginator(groups, 3)
+    page = request.GET.get('page')
+    try:
+       groups = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        groups = paginator.page(1)
+    except EmptyPage:
+       # If page is out of range (e.g. 9999), deliver
+        # last page of results.
+        groups = paginator.page(paginator.num_pages)
+    return render(request, 'students/groups_list.html',{'groups': groups})
 
 def groups_add(request):
      return HttpResponse('<h1>Group Add Form</h1>')
