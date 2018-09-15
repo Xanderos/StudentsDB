34c09637b4b00917179e2e4b803d06e6f818c1c2
Xanderos <aleksandrlex85@gmail.com>
2018-09-15 13:29:21 +0300

    admin validation

--- students/admin.py
+++ students/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 from .models.Students import Student
 from .models.Groups import Group
 from django.urls import reverse
+from django.forms import ModelForm, ValidationError
+
+class StudentFormAdmin(ModelForm):
+
+     def clean_student_group(self):
+         """Check if student is leader in any group.
+
+         If yes, then ensure it's the same as selected group."""
+         # get group where current student is a leader
+         groups = Group.objects.filter(leader=self.instance)
+         if len(groups) > 0 and self.cleaned_data['student_group'] != groups[0]:
+             raise ValidationError(u'Студент є старостою іншої групи.',
+                 code='invalid')
+
+         return self.cleaned_data['student_group']
 
 class StudentAdmin(admin.ModelAdmin):
      list_display = ['last_name', 'first_name', 'ticket', 'student_group']
@@ -12,9 +27,10 @@ class StudentAdmin(admin.ModelAdmin):
      list_per_page = 10
      search_fields = ['last_name', 'first_name', 'middle_name', 'ticket',
          'notes']
+     form = StudentFormAdmin
 
      def view_on_site(self, obj):
          return reverse('students_edit', kwargs={'pk': obj.id})
-# Register your models here.
-admin.site.register(Student,StudentAdmin)
+
+admin.site.register(Student, StudentAdmin)
 admin.site.register(Group)
